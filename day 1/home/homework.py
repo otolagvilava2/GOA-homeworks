7adbbb2ff28c9e56954a79a5bbb00d90011e2950
otolagvilava2 <otolagvilava2@gmai.com>
2026-02-03 18:37:06 +0400

    second commit

---
+++ day 1/home/homework.py
@@ -0,0 +1,42 @@
+name = "oto"
+last_name = "lagvilava"
+age = 16.2
+birth_year = 2009
+birth_month = "december"
+
+print(name)
+print(last_name)
+print(age)
+print(birth_year)
+print(birth_month)
+
+
+
+print(5+5)
+print(5-5)
+print(5/5)
+print(5//5)
+print(5*5)
+
+
+
+print(type(name))
+print(type(last_name))
+print(type(age))
+print(type(birth_year))
+print(type(birth_month))
+
+
+
+#+,-,/,//,* ამ ნიშნებს იმ დროს ვიყენებთ როდესაც გვინდა რომ მათემატიკური ოპერაცია შევასრულოთ
+
+#string ის დაწერა integer თან ან float თან არ შეიძლება.integer ის დაწერა float თან თავისუფლად შეიძლება 
+
+
+aaa = "oto"
+bbb = "lagvialava"
+ccc = 13.5 
+
+print(aaa,bbb)
+
+
