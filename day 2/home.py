67b62fda89c6fc2c68bb308878a85cb7ff378a2b
otolagvilava2 <otolagvilava2@gmai.com>
2026-02-05 17:55:50 +0400

    second commit

---
+++ day 2/home.py
@@ -0,0 +1,74 @@
+budget = 50
+print(budget + 20)
+#სიტყვა budget ის მაგივრად მოისაზრება ის რაც ცვლადში მივეცით მნიშვნელობა budget ს 
+
+
+name = "givi"
+name = "sandro"
+print(name)
+#ამ შემთხვევაში გამოიტანს sandros ს რადგან აუცილებელია დაიცვა თანმიმდევრობა 
+
+
+
+#Debbuging ნიშნავს კოდის წერის დროს დაშვებული შეცდომის გამოსწორებას.Debbuging მოიცავს 3 ეტაპს. კოდის წერას, კოდის გაშვებას და შესწორებას
+
+
+#print("home)
+#ერთი ბრჭყალი აკლია home ს და სწორი იქნება print("home")
+
+#1total = 50
+#აქ 1 არის ზედმეტი და და სწორი იქნება total =50
+
+#age = 20
+#print(Age)
+#აქ არასწორად არის ჩაწერილი ცვლადი და სწორად იქნება print(age)
+
+
+#age -> 25
+#აქ ისრის მაგივრად ტოლობის ნაშიანი უნდა (=)
+
+#print 10+19
+#აქ უნდა ფრჩხილები რომ შესრულდეს მოქმედება
+
+
+x = 15
+y = 20
+print(x+y)
+
+a=15
+c=3
+print(a//c)
+
+
+b = 20
+d= 5
+print(b*d)
+
+f = 8 
+p = 7
+print(f*p)
+
+a = 0
+b = 5
+print(a//b)
+
+
+
+
+name = "lionel"
+age = "messi"
+height = 170
+Preferred_foot = "left foot"
+Profession = "footballer"
+
+print(name , age,Profession,height,Preferred_foot)
+
+
+
+
+
+
+
+
+
+
