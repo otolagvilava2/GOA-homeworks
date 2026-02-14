eda33e65fef380071f1ab9dd3cb9e81b1b7f15bc
otolagvilava2 <otolagvilava2@gmai.com>
2026-02-14 14:40:56 +0400

    second commit

---
+++ day 5/class.py
@@ -0,0 +1,21 @@
+win = int(input("how many games did you win:"))
+ties = int(input("how many games did you tie: "))
+
+
+total_amount=(win*1)+(ties * 0.5)
+print(total_amount)
+
+
+
+
+
+
+
+
+
+bill = int(input("enter your bill amount:"))
+# tip = bill * 20 / 100
+# print(bill - tip)
+# print(tip)
+
+
---
+++ day 5/home.py
@@ -0,0 +1,62 @@
+# age = int(input("what's your age:"))
+# student = (input("are you a student:"))
+
+# print(age >= 18 and student == "yes")
+
+
+
+# a = float(input("enter a random number:"))
+# b = float(input("enter a random number:"))
+
+# print(a > 0 and b > 0 )
+# print(a > 100 or b > 100)
+
+# a = int(input("enter a random number:"))
+# print(a > 10 and a < 50 )
+# print(a < 0)
+
+
+# age = int(input("enter your age:"))
+# password = (input("enter your password:"))
+# a = "admin123"
+# print(age > 18 and password == a )
+
+
+# a = float(input("enter a random number:"))
+# b = float(input("enter a randon number:"))
+# c = float(input("enter a random number:"))
+
+# d= (a + b + c)/3
+# print(d>60)
+# print(type(d))
+# print(d)
+
+# speed = int(input("enter a speed amount:"))
+# print(speed > 120 or speed < 0)
+
+
+# height = float(input("enter your height:"))
+# weight = float(input("enter your weight:"))
+# print(height > 1.70 and weight < 90)
+# print(height < 150 or weight > 120)
+
+
+# a = int(input("enter a random number "))
+
+# print(a > 0 and a / 2)
+
+
+# a = input("enter a random number:")
+# print(type(a))
+# z = float(a)
+
+# print(z > 0 and z < 100)
+# print(type(z))
+
+
+
+age = int(input("enter your age"))
+balance = int(input("enter your balance"))
+status = (input("are you a vip?:"))
+print(age >= 18 and balance >=100 )
+print(age >=18 or status == "true")
