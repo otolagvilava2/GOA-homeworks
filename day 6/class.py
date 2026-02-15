f90ecf31c45060301e7c231a165be223d7468b70
otolagvilava2 <otolagvilava2@gmai.com>
2026-02-15 18:22:06 +0400

    second commit

---
+++ day 6/class.py
@@ -0,0 +1,26 @@
+# for i in range(21):
+#     print(i)
+
+
+# for i in range(26):
+#     print(i*2)
+
+# for i in range(10,0,-1):
+#     print(i)
+
+
+# a = input("enter your password:")
+
+# while a == "python123":
+#  print("acces granted")
+#  while a != "python123":
+#   print("you aren't logged in")
+
+
+a = (input("enter a random number:"))
+for i in a:
+    print(i)
+
+
+
+        
