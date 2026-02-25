1a7ad65016a14e24917fc858fc8aaa682c070d97
otolagvilava2 <otolagvilava2@gmai.com>
2026-02-25 21:00:58 +0400

    second commit

---
+++ day 10/home.py
@@ -0,0 +1,118 @@
+
+
+# list = []
+
+# a = int(input("number:"))
+# b = int(input("number:"))
+# c = int(input("number:"))
+
+# list.append(a)
+# list.append(b)
+# list.append(c)
+# print(list)
+
+
+
+# x = [5,6,1,3,6,1,7,1,]
+# print(x)
+# x.clear()
+# print(x)
+
+
+
+
+# original_list = [50,74,32,6,2454,]
+# copy_list = original_list.copy()
+
+
+# copy_list[3] = 100
+
+# print(original_list)
+# print(copy_list)
+
+
+
+
+
+# list = [5,1,6,13,6,17,5,2,4,7,2,4]
+
+
+# x = list.count(6)
+# print(x)
+
+
+
+# list = [6,9,10,53,71]
+# removed_item = list.pop(2)
+# print(list)
+# print(removed_item)
+
+
+
+# list = [5,88,163,67,356]
+# list.remove(67)
+# print(list)
+
+
+# list = [66,67,53,6,7,236,7,65]
+# list.reverse()
+# print(list)
+
+
+# list = [4,2,62,5,362,3,125,347,37,3,3,2,52,3]
+
+# list.sort()
+# print(list)
+# list.sort(reverse = True)
+# print(list)
+
+
+
+# list = []
+
+# a = int(input("number:"))
+# b = int(input("number:"))
+# c = int(input("number:"))
+# d = int(input("number:"))
+# e = int(input("number:"))
+
+# list.append(a)
+# list.append(b)
+# list.append(c)
+# list.append(d)
+# list.append(e)
+# list.sort()
+# print(list)
+
+
+
+# list = []
+
+# a = int(input("number"))
+# b = int(input("number"))
+# c = int(input("number"))
+# d = int(input("number"))
+
+# list.append(a)
+# list.append(b)
+# list.append(c)
+# list.append(d)
+# list.reverse
+# print(list)
+
+
+a =int(input("number"))
+b = int(input("number"))
+c = int(input("number"))
+d = int(input("number"))
+
+list.append(a)
+list.append(b)
+list.append(c)
+list.append(d)
+
+if list > 100:
+    list.clear
+else:
+    list
+print(list)    
