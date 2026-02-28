96dd14419499e6888072722a14b12a0b4e55c715
otolagvilava2 <otolagvilava2@gmai.com>
2026-02-28 14:29:47 +0400

    second commit

---
+++ day 11/home.py
@@ -0,0 +1,117 @@
+#1
+ # list = []
+
+# a = int(input("number:"))
+# b = int(input("number:"))
+# c = int(input("number:")) 
+# d = int(input("number:")) 
+# e = int(input("number:"))
+
+# list.append(a)
+# list.append(b)
+# list.append(c)
+# list.append(d)
+# list.append(e)
+
+# list.sort()
+# print(list)
+
+# ამოღებული_რიცხვი = list.pop(0)
+
+# print("ამოღებული რიცხვი არის", ამოღებული_რიცხვი)
+# print(list)
+
+
+#2
+# a = int(input("number:"))
+# b = int(input("number:"))
+# c = int(input("number:")) 
+# d = int(input("number:")) 
+# e = int(input("number:"))
+# f = int(input("number:"))
+
+# list = []
+
+
+# list.append(a)
+# list.append(b)
+# list.append(c)
+# list.append(d)
+# list.append(e)
+# list.append(f)
+
+
+# oo =  list.count(10)
+
+# if oo > 2:
+#     list.remove(10)
+#     print("ერთი ათი წაიშალა")
+# else:
+#     print(list)
+
+
+#3
+# list =[6,7,8,9]
+# copy_list = list.copy()
+# copy_list.append(100)
+# copy_list.sort(reverse=True)
+# print(copy_list)
+
+
+#4
+# a = int(input("number:"))
+# b = int(input("number:"))
+# c = int(input("number:")) 
+# d = int(input("number:")) 
+# e= int(input("number:"))
+
+# list = [] 
+
+# list.append(a)
+# list.append(b)
+# list.append(c)
+# list.append(d)
+# list.append(e)
+
+# list.sort()
+# if a or b or c or d or e < 0:
+#     list.clear
+# else:
+#     print(list)
+
+
+#5
+
+# a = int(input("number:"))
+# b = int(input("number:"))
+# c = int(input("number:")) 
+# d = int(input("number:")) 
+# e = int(input("number:"))
+# f = int(input("number:"))
+# g = int(input("number:"))
+
+
+# list = []
+# list.append(a)
+# list.append(b)
+# list.append(c)
+# list.append(d)
+# list.append(e)
+# list.append(f)
+# list.append(g)
+# list.sort()
+# a = list.pop()
+# print(list)
+
+
+#6
+# list = [6,8,77,1,5,6]
+# print(list[0:3])
+
+
+#7
+# list = [5,7,1,8,9,0,1,22]
+# print(list[-4:9])
+#8
+list = [77,55,11,4,9,45,67]
+print(list[2:5])
