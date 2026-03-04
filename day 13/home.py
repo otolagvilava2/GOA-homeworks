1db355eabc418b5fd56471ebfc50b953e852c428
otolagvilava2 <otolagvilava2@gmai.com>
2026-03-04 16:31:06 +0400

    second commit

---
+++ day 13/home.py
@@ -0,0 +1,68 @@
+
+
+
+# def sum(n):
+#     total = 0
+#     for i in range(1,n+1):
+#         total += i
+#     return total    
+# print(sum(7))        
+
+
+
+
+# def kenti(n):
+#     count_odd = 0
+#     for i in range(1,n+1):
+#         if i % 2 != 0:
+#             count_odd +=1
+#     return count_odd
+# print(kenti(13))        
+
+
+# def jami(list):
+#     total = 0
+#     for i in list:
+#         total += i 
+#     return total
+
+# list = [7,99,1,5,6]
+# print(jami(list))
+
+
+
+# def raodenoba(numbers, target):
+#     count = 0
+    
+#     for num in numbers:
+#         if num == target:
+#             count += 1
+            
+#     return count
+
+# sia = [6,1,5,9,3,5,9,10,]
+# print(raodenoba(sia,9))
+
+
+
+
+# def gamravleba(n):
+#     kubi = (n * n * n)
+#     return kubi
+     
+# print(gamravleba(5))
+
+    
+
+
+# def mcire(a,b):
+#     if a < b:
+#         return a
+#     else:
+#         return b
+    
+# print(mcire(10,7))
+
+word = "run"
+word[0] = "f"
+print(word)
