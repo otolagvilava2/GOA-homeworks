4b24d24ff13e26d0e683a1dc00c1000e59e37b49
otolagvilava2 <otolagvilava2@gmai.com>
2026-05-17 19:23:31 +0400

    second commit

--- day 14/class/class.py
+++ day 14/class/class.py
@@ -1,6 +1,9 @@
-print("SaXli".lower())
-print("HOme".upper())
-print("bAg".capitalize())
+# print("SaXli".lower())
+# print("HOme".upper())
+# print("bAg".capitalize())
 
-print("Paper".find("e"))
-print("paper".find("s"))
+# print("Paper".find("e"))
+# print("paper".find("s"))
+
+
+
